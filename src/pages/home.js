7804db13f96f4750e264fe02731472c8360367c9
teshigahara <crnhrv@gmail.com>
2021-02-12 20:34:22 +0000

    add hero to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,30 @@
 import { HeaderContainer } from '../containers/header';
+import { ROUTES } from '../constants/routes';
+import { Background, Hero } from '../components';
+import backgroundImg from '../img/tea-leaf.png';
 
-export const Home = ({ children, ...restProps }) => {
-  return <HeaderContainer active="home" />;
+export const Home = () => {
+  return (
+    <>
+      <HeaderContainer active={ROUTES.HOME.title} />
+      <Hero>
+        <Hero.Title>
+          The
+          <br />
+          Perfect
+          <br />
+          <Hero.Accent>Tea</Hero.Accent>
+        </Hero.Title>
+        <Hero.Pane>
+          <Hero.Subtitle>Find your brew today.</Hero.Subtitle>
+          <Hero.Button>browse</Hero.Button>
+        </Hero.Pane>
+        <Hero.Pane></Hero.Pane>
+        <Hero.Pane></Hero.Pane>
+      </Hero>
+      <Background>
+        <Background.Image src={backgroundImg} />
+      </Background>
+    </>
+  );
 };
