7e856bc3a876668c716ff0ea18e49cc8a1105e83
teshigahara <crnhrv@gmail.com>
2021-02-18 19:18:06 +0000

    add components to index list

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as Header } from './header';
 export { default as Background } from './background';
 export { default as Hero } from './hero';
+export { default as Card } from './card';
+export { default as Sidebar } from './sidebar';
+export { default as Tagline } from './tagline';
