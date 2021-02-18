dcb80763c3dea717f09c028e422f260d6c8b24ee
teshigahara <crnhrv@gmail.com>
2021-02-18 19:17:48 +0000

    add basic tagline layout

---
+++ src/components/tagline/index.js
@@ -0,0 +1,15 @@
+import { Title, Subtitle, Accent } from './styles/tagline';
+
+const Tagline = ({ children, ...restProps }) => {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Tagline.Subheading = ({ children, ...restProps }) => {
+  return <Subtitle {...restProps}>{children}</Subtitle>;
+};
+
+Tagline.Accent = ({ children, ...restProps }) => {
+  return <Accent {...restProps}>{children}</Accent>;
+};
+
+export default Tagline;
---
+++ src/components/tagline/styles/tagline.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1``;
+
+export const Subtitle = styled.p``;
+
+export const Accent = styled.span``;
