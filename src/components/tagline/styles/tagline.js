fcbb930c2186c3e79b43caf6345c5429b068f8ef
teshigahara <crnhrv@gmail.com>
2021-02-19 18:47:51 +0000

    finish tag layout

--- src/components/tagline/styles/tagline.js
+++ src/components/tagline/styles/tagline.js
@@ -1,7 +1,20 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+  grid-area: title;
+  margin-bottom: 1em;
+  text-align: center;
+  font-size: 2.3rem;
+  color: var(--secondary);
+
+  @media (min-width: 900px) {
+    font-size: 3rem;
+    text-align: center;
+  }
+`;
 
 export const Subtitle = styled.p``;
 
-export const Accent = styled.span``;
+export const Accent = styled.span`
+  color: var(--accent);
+`;
