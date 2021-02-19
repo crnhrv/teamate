836192b8cb30bf5de40e5f2f37ea9a360d7c16c9
teshigahara <crnhrv@gmail.com>
2021-02-19 18:48:52 +0000

    finish card layout

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -4,11 +4,51 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  max-width: 100%;
+  cursor: pointer;
 `;
 
-export const Header = styled.h2``;
+export const Group = styled.div`
+  grid-area: card;
+  display: grid;
+  gap: 1em;
+  place-content: center;
+  max-width: 150px;
+  margin: 0 auto;
 
-export const Text = styled.p``;
+  @media (min-width: 700px) {
+    grid-template-columns: repeat(auto-fit, minmax(0px, 100px));
+    place-items: center;
+    margin: unset;
+    gap: 3em;
+    max-width: 1200px;
+  }
+
+  @media (min-width: 800px) {
+    grid-template-columns: repeat(auto-fit, minmax(0px, 200px));
+  }
+
+  @media (min-width: 1400px) {
+    place-self: center;
+  }
+`;
+
+export const Header = styled.h2`
+  align-self: start;
+  margin: 0;
+  margin-top: 0.2em;
+  margin-left: 0.7rem;
+`;
+
+export const Text = styled.p`
+  align-self: start;
+  margin-top: 0;
+  margin-left: 0.7rem;
+  margin-bottom: 2em;
+  color: var(--accent);
+  font-weight: bold;
+  font-size: 1.1rem;
+`;
 
 export const Image = styled.img`
   max-width: 100%;
