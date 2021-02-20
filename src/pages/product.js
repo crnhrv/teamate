96ba1b259088a90dea31ffb84432730236c18f25
teshigahara <crnhrv@gmail.com>
2021-02-20 18:29:02 +0000

    add product page

---
+++ src/pages/product.js
@@ -0,0 +1,38 @@
+import { HeaderContainer } from '../containers/header';
+import { useParams, useHistory } from 'react-router-dom';
+import { HEADER_ROUTES } from '../constants/routes';
+import { Card } from '../components';
+import { TEAS } from '../constants/teas';
+
+export const Product = (props) => {
+  const { id } = useParams();
+  const { goBack } = useHistory();
+  const tea = Object.values(TEAS).find((tea) => tea.id === Number(id));
+
+  return (
+    <>
+      <HeaderContainer active={HEADER_ROUTES.BROWSE.title} />
+      <Card.Pane>
+        <Card.Section>
+          <Card.Button onClick={goBack}>Back</Card.Button>
+          <Card.Button>Add to Cart</Card.Button>
+        </Card.Section>
+        <Card.Header>
+          {tea.name} - <Card.Accent>£{tea.price}</Card.Accent>
+        </Card.Header>
+        <Card.Image src={tea.img} />
+        <Card.Text>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </Card.Text>
+      </Card.Pane>
+    </>
+  );
+};
