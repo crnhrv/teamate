28f8b02fdd4e409aafafeff57bec7079fb4ce5e6
teshigahara <crnhrv@gmail.com>
2021-02-20 23:12:11 +0000

    add cart page

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,6 +1,46 @@
 import { HeaderContainer } from '../containers/header';
 import { HEADER_ROUTES } from '../constants/routes';
+import { CartContainer } from '../containers/cart';
+import { useContext } from 'react';
+
+import { CartContext } from '../contexts/cart_context';
+import { Tagline, Card } from '../components';
+import { CartPageWrapper } from './styles/cart';
+import { useHistory } from 'react-router';
+import { roundDecimal } from '../utils/round_decimal';
 
 export const Cart = () => {
-  return <HeaderContainer active={HEADER_ROUTES.CART.title} />;
+  const { goBack } = useHistory();
+
+  const { cartItems } = useContext(CartContext);
+  const total = cartItems.reduce(
+    (acc, curr) => acc + curr.price * curr.count,
+    0
+  );
+  const displayTotal = roundDecimal(total);
+  const headerText = cartItems.length
+    ? 'Almost Done Brewing..'
+    : "Nothing Brewin'";
+  return (
+    <>
+      <HeaderContainer active={HEADER_ROUTES.CART.title} />
+      <CartPageWrapper>
+        <Tagline margin={'1em 0 0.4em 0'} align="center">
+          {headerText}
+          {total ? (
+            <Tagline.Subtitle>
+              Total: <Tagline.Accent>£{displayTotal}</Tagline.Accent>
+            </Tagline.Subtitle>
+          ) : null}
+        </Tagline>
+        {cartItems.length ? (
+          <CartContainer />
+        ) : (
+          <Card.Button onClick={goBack} size="large">
+            Back
+          </Card.Button>
+        )}
+      </CartPageWrapper>
+    </>
+  );
 };
