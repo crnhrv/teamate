573d221650605398a134b626fc6f8d92b9fae62b
teshigahara <crnhrv@gmail.com>
2021-02-20 18:50:39 +0000

    add cart context

--- src/app.js
+++ src/app.js
@@ -1,27 +1,32 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { createElement } from 'react';
-
 import { GlobalStyle } from './global_style';
 import { HEADER_ROUTES, PRODUCT_ROUTE } from './constants/routes';
+import { CartContext } from './contexts/cart_context';
+import { useCart } from './hooks/use_cart';
 
 function App() {
-  return (
-    <Router>
-      <GlobalStyle />
+  const { addToCart, removeFromCart, cartItems } = useCart();
 
-      <Switch>
-        {Object.values(HEADER_ROUTES).map((route) => {
-          return (
-            <Route key={route.path + route.title} exact path={route.path}>
-              {createElement(route.component)}
-            </Route>
-          );
-        })}
-        <Route path={PRODUCT_ROUTE.path}>
-          {createElement(PRODUCT_ROUTE.component)}
-        </Route>
-      </Switch>
-    </Router>
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+      <Router>
+        <GlobalStyle />
+
+        <Switch>
+          {Object.values(HEADER_ROUTES).map((route) => {
+            return (
+              <Route key={route.path + route.title} exact path={route.path}>
+                {createElement(route.component)}
+              </Route>
+            );
+          })}
+          <Route path={PRODUCT_ROUTE.path}>
+            {createElement(PRODUCT_ROUTE.component)}
+          </Route>
+        </Switch>
+      </Router>
+    </CartContext.Provider>
   );
 }
 
